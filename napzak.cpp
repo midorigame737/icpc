6f81faa7a7254a0be9189f1efcdfc2a34bed4c9a
midorigame737 <midorigame6@gmail.com>
2022-09-02 18:57:44 +0900

    ナップザックのリファクタリングとコメント文追記

--- napzak.cpp
+++ napzak.cpp
@@ -5,11 +5,17 @@ int main(){
     int weight[110],value[110];
     int dp[110][10010];
     cin>>n>>MaxWeight;
-    for(int i=0;i<n;++i)cin>>value[i]>>weight[i];
-    for(int w=0;w<=MaxWeight;++w)dp[0][w]=0;
     for(int i=0;i<n;++i){
-        for(int W=0;W<=MaxWeight;++W){
-            if(W>=weight[i])dp[i+1][W]=max(dp[i][W-weight[i]]+value[i],dp[i][W]);
+        cin>>value[i]>>weight[i];
+    }
+    for(int w=0;w<=MaxWeight;++w){
+        dp[0][w]=0;
+    }
+    for(int i=0;i<n;++i){
+        for(int W=0;W<=MaxWeight;++W){//0から各重さの期待値を見ていく
+            if(weight[i]<W){//現在見ている上限値の重さ入るようであればナップザックに入れる
+                dp[i+1][W]=max(dp[i][W-weight[i]]+value[i],dp[i][W]);
+            }
             else dp[i+1][W]=dp[i][W];
         }
     }
