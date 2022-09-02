e8c2c354bc2669e7059efaf9584333a862b468ee
midorigame737 <midorigame6@gmail.com>
2022-09-02 19:07:23 +0900

    コメント間違ってたので修正

--- napzak.cpp
+++ napzak.cpp
@@ -13,8 +13,8 @@ int main(){
     }
     for(int i=0;i<n;++i){
         for(int W=0;W<=MaxWeight;++W){//0から各重さの期待値を見ていく
-            if(weight[i]<W){//現在見ている上限値の重さ入るようであればナップザックに入れる
-                dp[i+1][W]=max(dp[i][W-weight[i]]+value[i],dp[i][W]);
+            if(weight[i]<W){//現在見ている上限値の重さ入るようであれば
+                dp[i+1][W]=max(dp[i][W-weight[i]]+value[i],dp[i][W]);//現在の重さの価値の最大値と、現在入れるか検討しているものと過去に作成した重さの価値の和と比べる
             }
             else dp[i+1][W]=dp[i][W];
         }
