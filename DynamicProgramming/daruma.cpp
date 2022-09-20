2803af7ce1443a06e0ef5d197e69ceb32b4a4a53
midorigame737 <midorigame6@gmail.com>
2022-09-20 23:36:13 +0900

    関数recのインターフェース変更　darumaと動的計画法の領域をグローバルに

--- DynamicProgramming/daruma.cpp
+++ DynamicProgramming/daruma.cpp
@@ -1,27 +1,30 @@
 #include<bits/stdc++.h>
 using namespace std;
-int rec(int l,int r,vector<vector<int>>dp,vector<int>daruma){
+vector<int>daruma;
+vector<vector<int>>dp;
+int rec(int l,int r){
         if(dp[l][r]!=-1)return dp[l][r];
         if(dp[l][r]<=1)return 0;
         int res=0;
-        //パターン1
-        if(abs(daruma[l]-daruma[r-1])<=1&&rec(l+1,r-1,dp,daruma)==r-l-2){
+        //繝代ち繝ｼ繝ｳ1
+        if(abs(daruma[l]-daruma[r-1])<=1&&rec(l+1,r-1)==r-l-2){
             res=r-1;
         }
-        //パターン2
+        //繝代ち繝ｼ繝ｳ2
         for(int mid=l+1;mid<=r-1;mid++){
-            res=max(res,rec(l,mid,dp,daruma)+rec(mid,r,dp,daruma));
+            res=max(res,rec(l,mid)+rec(mid,r));
         }
         return dp[l][r]=res;
     }
 int main(){
     int n;
     cin>>n;
-    vector<int>daruma(n);
+    daruma.resize(n);
     for(int i=0;i<n;++i){
         cin>>daruma[i];
     }
-    vector<vector<int>>dp(n,vector<int>(n));
+    dp.resize(n,vector<int>(n));
+
     for(int i=0;i<n;++i){
         for(int j=0;j<n;++j){
             dp[i][j]=0;
