f88efc41ad31d952a315e6d7d14fa052a5db0ccc
midorigame737 <midorigame6@gmail.com>
2023-02-07 00:18:44 +0900

    C問題動くようにした

--- ABC288/C.cpp
+++ ABC288/C.cpp
@@ -1,39 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 int n,m;
-vector<bool>edges;
-vector<int>startp;
-vector<int>endp;
-bool check(int startp,int endp,int goal){
-    int count=0;
-    for(int i=0;i<n;++i){//頂点なめる
-        for(int j=0;j<m;++j){//辺なめる
-            if(startp[j]==i){
-                count++;
-            }
-        }
-    }
-    if(count==0)return false;
-    check()
-}
-
 
 int main(){
     int n,m;
-       cin>>n>>m;
-    edges.resize(n);
-    startp.resize(m);
-    endp.resize(m);
+    cin>>n>>m;
+    vector<vector<int>>graph(n,vector<int>());
     for(int i=0;i<m;++i){
-        int a,b;
-        cin>>startp[i]>>endp[i];
+        int u,v;
+        cin>>u>>v;
+        u--;v--;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
     }
-    for(int i=0;i<n;++i){//頂点なめる
-        for(int j=0;j<m;++j){//辺なめる
-            if(startp[j]==i){
-                check(i,endp[i],i);
+    vector<bool>used(n);
+    int s=0;
+    for(int i=0;i<n;++i){
+        if(!used[i]){
+            s++;
+            queue<int>que;
+            que.push(i);
+            while(!que.empty()){
+                int q=que.front();que.pop();
+                for(int v:graph[q]){
+                    if(!used[v]){
+                        used[v]=true;
+                        que.push(v);
+                    }
+                }
             }
         }
     }
+    cout<<m-n+s<<'\n';
     return 0;
 }
