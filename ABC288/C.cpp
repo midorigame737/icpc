c95366bd3ae02aad57c080e0312bdc337584a8c8
midorigame737 <midorigame6@gmail.com>
2023-02-10 06:38:56 +0900

    コードの解説の追加と余分な出力を条件付コンパイルに変更

--- ABC288/C.cpp
+++ ABC288/C.cpp
@@ -10,11 +10,14 @@ int main(){
         int u,v;
         cin>>u>>v;
         u--;v--;
-        graph[u].push_back(v);//一つ目のインデックスで各頂点（連結を持つ）を表し、二つ目のインデックスの中身がつながってる頂点を表す
+        /*一つ目のインデックスで各頂点（連結を持つ）を表し、
+        二つ目のインデックスの中身がつながってる頂点を表す*/
+        graph[u].push_back(v);
         graph[v].push_back(u);
     }
     vector<bool>used(n);
     int s=0;//グラフの連結成分の数
+    #ifdef DEBUG
     cout<<"graph"<<endl;
     for(int i=0;i<n;++i){
         cout<<"i="<<i<<endl;
@@ -22,25 +25,35 @@ int main(){
             cout<<j<<" ";
         }
         cout<<endl;
+
     }
     cout<<endl;
+    #endif
     for(int i=0;i<n;++i){//頂点なめる
         if(!used[i]){
             s++;
             queue<int>que;
             que.push(i);
-            while(!que.empty()){
+            while(!que.empty()){//連結している頂点がなくなるか、連結している頂点がすでに通過済みのものだけになるまでループ
                 int q=que.front();que.pop();
+                #ifdef DEBUG
                 cout<<"q="<<q<<endl;
-                for(int v:graph[q]){
+                #endif
+                for(int v:graph[q]){//現在調べている頂点と連結している頂点に対して一通り処理する
                     if(!used[v]){
                         used[v]=true;
-                        cout<<"v"<<v;
-                        que.push(v);
+                        #ifdef DEBUG 
+                        cout<<"v"<<v; 
+                        #endif
+                        que.push(v);//後で調べる頂点に追加
                     }
                 }
             }
         }
+        #ifdef DEBUG
+        for(bool a:used){cout<<a<<" ";}
+        cout<<endl;
+        #endif
     }
     cout<<m-n+s<<'\n';
     return 0;
