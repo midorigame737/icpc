c3c14b2b0c47a41e304c249ab63d64b2d2fdf4c7
midorigame737 <midorigame6@gmail.com>
2023-04-11 07:41:43 +0900

    D問題解いた

--- Atc/ABC297/D.cpp
+++ Atc/ABC297/D.cpp
@@ -1,21 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-long long result=0;
-long long Subtractions(long long a,long long b){
-    if(a==b)return result;
+
+long long Subtractions(long long a,long long b,long long result){
+    //cout<<"a="<<a<<"b="<<b<<"result="<<result<<endl;
+    if(a==b){
+        //cout<<"a="<<a<<"b="<<b<<"result="<<result;
+        return result;
+    }
+    //TODOもっと簡潔にかけるのであとで直す
     if(a<b){
-        result+=b/a;
-        b=b%a;
-        return Subtractions(a,b);
+        if(b%a!=0){
+            result+=b/a;
+            b=b%a;
+        }
+        else{
+            result+=(b-a)/a;
+            b=a;
+        }
+        return Subtractions(a,b,result);
     }
     else{
-        result+=a/b;
-        return Subtractions(a,b);
+        if(a%b!=0){
+            result+=a/b;
+            a=a%b;
+        }
+        else{
+            result+=(a-b)/b;
+            a=b;
+        }
+        return Subtractions(a,b,result);
     }
 }
 int main(){
     long long a,b;
     cin>>a>>b;
-    cout<<Subtractions(a,b);
+    long long result=Subtractions(a,b,0);
+    cout<<result;
     return 0;
 }
