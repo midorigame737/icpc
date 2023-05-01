90a2857ed035aaf4b1a5bc4f0fa67af32b35fd25
midorigame737 <midorigame6@gmail.com>
2023-05-01 10:12:53 +0900

    ABC300 B問題リファクタリング

--- Atc/ABC300/B.cpp
+++ Atc/ABC300/B.cpp
@@ -1,49 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-string left_shift(string s){
-    string result;
-    for(int i=0;i<s.size();++i){
-        if(i==0){
-            result[s.size()-1]=s[i];
-        }
-        else{
-            result[i-1]=s[i];
-        }
-    }
-    return result;
-}
-vector<string> up_shift(vector<string> s){
-    vector<string> result;
-    for(int i=0;i<s.size();++i){
-        if(i==0){
-            result[s.size()-1]=s[i];
-        }
-        else{
-            result[i-1]=s[i];
-        }
-    }
-    return result;
-}
+//#define DEBUG
+
 int main(){
     int h,w;
     cin>>h>>w;
     vector<string>A(h),B(h);
     for(string& s:A)cin>>s;
     for(string& s:B)cin>>s;
-    for(int s=0;s<h;s++){
-        for(int t=0;t<w;++t){
-            bool ok=true;
-            for(int i=0;i<h;++i){
-                for(int j=0;j<w;++j){
-                    if(A[(i-s+h)%h][(j-t+w)%w]!=B[i][j])ok=false;
+
+    for(int s=0;s<h;s++){//横シフトループ
+        for(int t=0;t<w;++t){//縦シフトループ
+            bool result=true;
+            for(int y=0;y<h;++y){//縦ループ
+                for(int x=0;x<w;++x){//横ループ
+                    #ifdef DEBUG
+                    cout<<"i="<<i<<"s="<<s<<"h="<<h<<"index"<<(i-s+h)%h<<endl;
+                    #endif
+                    int shiftedY=(y-s+h)%h;
+                    int shiftedX =(x-t+w)%w;
+                    result = A[shiftedY][shiftedX]==B[y][x];
                 }
             }
-            if(ok){
+            if(result){
                 cout<<"Yes";
                 return 0;
             }
         }
     }
-cout<<"No"<<endl;
+    cout<<"No"<<endl;
     return 0;
 }
