dde520ace816e02468a04c356c3bbc8a3a4f6b81
midorigame737 <midorigame6@gmail.com>
2023-05-14 09:28:00 +0900

    ABC301 B問題

--- Atc/ABC301/B.cpp
+++ Atc/ABC301/B.cpp
@@ -1,51 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define DEBUG
+//#define DEBUG
 int main(){
     int n;
     cin>>n;
     vector<int>v(n);
     for(int& a:v)cin>>a;
-    int size=n;
-    for(int i=0;i<size-1;++i){
-        if(v[i+1]==v[i]-1||v[i+1]==v[i]+1)continue;
-        if(v[i]>v[i+1]){
-            auto it=v.begin()+i+1;
-            int stop=v[i+1];
-            for(int j=v[i]-1;j>stop;--j){
-                #ifdef DEBUG
-                cout<<"j="<<j<<"v[i+1]="<<v[i+1]<<endl;
-                cout<<"it="<<v.begin()-it<<endl;
-                for(int& a:v)cout<<a<<" ";
-                cout<<endl;
-                #endif
-                v.insert(it,j);
-                it++;
-                
-                #ifdef DEBUG
-                cout<<"it="<<v.begin()-it<<endl;
-                cout<<"stop="<<stop<<endl;
-                for(int& a:v)cout<<a<<" ";
-                cout<<endl;
-                #endif
-            }
-            size=v.size();
-        }
-        else{
-            auto it=v.begin()+i+1;
-            int stop=v[i+1];
-            for(int j=v[i]+1;j<stop;++j){
-                #ifdef DEBUG
-                cout<<"it="<<it-v.begin()<<endl;
-                cout<<"stop="<<stop<<endl;
-                for(int& a:v)cout<<a<<" ";
-                cout<<endl;
-                #endif
-                v.insert(it,j);
-                it++;
-            }
-            size=v.size();
-        }
+    
+    for(int i=1;i<v.size();++i){
+        #ifdef DEBUG
+        cout<<"i="<<i<<"value="<<v[i]<<endl;
+        #endif
+        if(v[i-1]+1<v[i])v.insert(v.begin()+i,v[i-1]+1);//前側にinsertするから今見てる要素からずれない
+        if(v[i-1]-1>v[i])v.insert(v.begin()+i,v[i-1]-1);
     }
     for(int& a:v)cout<<a<<" ";
     return 0;
