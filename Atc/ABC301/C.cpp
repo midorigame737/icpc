9cdc807313555b127136507b88f1934b06605959
midorigame737 <midorigame6@gmail.com>
2023-05-14 09:28:31 +0900

    ABC301 C問題

--- Atc/ABC301/C.cpp
+++ Atc/ABC301/C.cpp
@@ -1,35 +1,44 @@
 #include<bits/stdc++.h>
 using namespace std;
+//#define DEBUG
 int main(){
     string s,t;
     cin>>s>>t;
     map<char,int>sCount,tCount;
-    for(int i=0;i<s.size();++i){
-        sCount[s[i]]++;
-    }
-    for(int i=0;i<t.size();++i){
-        tCount[t[i]]++;
-    }
+    for(char& c:s)sCount[c]++;
+    for(char& c:t)tCount[c]++;
+    
     string atc="atcoder";
-    int count=0;
-    for(const auto&[key,value]:sCount){
-        if(key=='@')continue;
-        if(value!=tCount[key]){
-            bool flag=false;
-            for(auto &s :atc){
-                if(key==s){
-                    flag=true;
-                    count+=abs(value-tCount[key]);
-                }
+    for(char& key:atc){
+        if(tCount[key]>sCount[key]){
+            int difference = tCount[key]-sCount[key];
+            sCount['@']-=difference;
+            sCount[key]+=difference;
+            if(sCount['@']<0){
+                cout<<"No";
+                return 0;
             }
-            if(!flag){
+        }
+        if(tCount[key]<sCount[key]){
+            int difference=sCount[key]-tCount[key];
+            tCount['@']-=difference;
+            tCount[key]+=difference;
+            if(tCount['@']<0){
                 cout<<"No";
                 return 0;
             }
         }
     }
-    if(count==0||count<tCount['@']||count<sCount['@'])cout<<"Yes";
-    else cout<<"No";
-
+    for(char& c:s){
+        if(sCount[c]!=tCount[c]){
+            #ifdef DEBUG
+            cout<<"key="<<c<<endl;
+            cout<<"svaue="<<sCount[c]<<"tvalue="<<tCount[c]<<endl;
+            #endif
+            cout<<"No";
+            return 0;
+        }
+    }
+    cout<<"Yes";
     return 0;
 }
